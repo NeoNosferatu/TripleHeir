99301f007e86a4c0a891216b704eb9d7815dd3f0
1 <1@LAPTOP-FD3T0C4D>
2023-03-03 17:35:19 +0700

    Добавьте файлы проекта.

---
+++ TripleHeir.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleHeir
+{
+    class TripleHeir
+    {
+        static void Main(string[] args)
+        {
+            Char number = new Char();
+            String name = new String();
+            Int age = new Int();
+            number.number();
+            name.name();
+            age.age();
+            Console.ReadLine();
+        }
+    }
+    public class Char
+    {
+        public char Number;
+        public Char()
+        {
+            Number = '1';
+        }
+        public void number()
+        {
+            Console.WriteLine($"Номер: {Number}");
+        }
+    }
+    public class String : Char
+    {
+        public string Name;
+        public String()
+        {
+            Name = "Maks";
+        }
+        public void name()
+        {
+            Console.WriteLine($"Имя: {Name}");
+        }
+    }
+    public class Int : String
+    {
+        public int Age;
+        public Int()
+        {
+            Age = 20;
+        }
+        public void age()
+        {
+            Console.WriteLine($"Возраст: {Age}");
+        }
+    }
+}
+
